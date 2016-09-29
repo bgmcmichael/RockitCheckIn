7888f0cc02023e96059cfebfdc73425e09c6d957
benjamin mcmichael <benjamin mcmichael>
2016-09-29 17:42:38 -0400

    init commit

---
+++ src/main/java/com/tiy/RocketCheckIn/RocketCheckInApplication.java
@@ -0,0 +1,12 @@
+package com.tiy.RocketCheckIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketCheckInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketCheckInApplication.class, args);
+	}
+}
